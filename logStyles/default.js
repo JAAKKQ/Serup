6fb9a469418ae1dddd9a76c4f6c14551530cc08b
Jaakko <mail@r3ne.net>
2022-05-01 18:53:22 +0300

    Update default.js

--- logStyles/default.js
+++ logStyles/default.js
@@ -1,43 +1,43 @@
 var COMport = "";
 
 module.exports = function (COMport) {
-	return {
-		COMport: COMport,
+    return {
+        COMport: COMport,
 
-		init: function () {
+        init: function () {
             console.log('\x1b[32m%s\x1b[0m', "-------------------------Serup-------------------------");
             console.log('YOU' + ': Serial Port Set');
-		},
+        },
 
         portOpen: function () {
             console.log('YOU' + ': Serial Port Open');
-		},
-        
+        },
+
         callSend: function () {
             console.log('YOU' + ': Callback received. Sending one back.');
-		},
+        },
 
         callSendError: function (Err) {
             console.log('YOU' + ': Error sending callback: ', Err);
-		},
-        
+        },
+
         divider: function () {
             console.log('\x1b[32m%s\x1b[0m', "-------------------------------------------------------");
-		},
-        
+        },
+
         took: function (data) {
             console.log(COMport + ': ' + data);
-		},
+        },
 
         otherData: function (data) {
-		},
-        
+        },
+
         start: function () {
             console.log('YOU' + ': Start command send.');
-		},
-        
+        },
+
         startError: function (Err) {
             console.log('YOU' + ': Error while sending start: ', Err);
-		}
-	}
+        }
+    }
 };
